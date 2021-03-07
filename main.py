86f29acb59ffc4226edc50cbaffa12e9069087ba
lucas <lu.zatonski34@gmail.com>
2021-03-07 12:27:46 +0100

    The coffee machine counts how many cups of coffee can make with ingredients, which user typed

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-print("Write how many cups of coffee you will need:")
+print("Write how many ml of water the coffee machine has: ")
+water = int(input())
+print("Write how many ml of milk the coffee machine has: ")
+milk = int(input())
+print("Write how many grams of coffee beans the coffee machine has: ")
+beans = int(input())
+print("Write how many cups of coffee you will need: ")
 how_many = int(input())
-water = how_many * 200
-milk = how_many * 50
-beans = how_many * 15
-if how_many == 1:
-    print(f"For {how_many} cup of coffee you will need:")
-else:
-    print(f"For {how_many} cups of coffee you will need:")
-print(f"{water} ml of water")
-print(f"{milk} ml of milk")
-print(f"{beans} g of coffee beans")
+
+
+maximally_water = water//200
+maximally_milk = milk//50
+maximally_beans = beans//15
+how_maximally = min(maximally_water, maximally_milk, maximally_beans)
+how_more = how_maximally - how_many
+
+if how_maximally == how_many:
+    print("Yes, I can make that amount of coffee ")
+elif how_maximally < how_many:
+    if how_maximally == 1:
+        print("No, I can make only {} cup of coffee".format(how_maximally))
+    else:
+        print("No, I can make only {} cups of coffee".format(how_maximally))
+elif how_maximally > how_many:
+    print("Yes, I can make that amount of coffee (and even {} more than that)".format(how_more))
+
